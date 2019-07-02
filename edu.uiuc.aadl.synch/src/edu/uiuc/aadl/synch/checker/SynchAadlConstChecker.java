0966db708abd789414a6d99fdde45526d1f726e4
Sharon Kim <ksron@postech.ac.kr>
2019-07-02 11:49:30 +0900

    update checker

--- edu.uiuc.aadl.synch/src/edu/uiuc/aadl/synch/checker/SynchAadlConstChecker.java
+++ edu.uiuc.aadl.synch/src/edu/uiuc/aadl/synch/checker/SynchAadlConstChecker.java
@@ -17,7 +17,7 @@ public class SynchAadlConstChecker extends AadlProcessingSwitchWithProgress {
 
 	//TODO: check input adators for multirate connections (e.g., existance, fast/slow types, etc.)
 	//TODO: should initial values be a vector for the fast machine?, or given by the related input adaptors??
-	
+
 	public SynchAadlConstChecker(IProgressMonitor pm, AnalysisErrorReporterManager errMgr) {
 		super(pm, errMgr);
 	}
@@ -25,7 +25,7 @@ public class SynchAadlConstChecker extends AadlProcessingSwitchWithProgress {
 	@Override
 	protected void initSwitches() {
 		instanceSwitch = new InstanceSwitch<String>() {
-			
+
 			@Override
 			public String caseComponentInstance(ComponentInstance ci) {
 				checkCompSynch(ci);
@@ -34,71 +34,111 @@ public class SynchAadlConstChecker extends AadlProcessingSwitchWithProgress {
 				monitor.worked(1);
 				return DONE;
 			}
-			
+
 			@Override
 			public String caseConnectionInstance(ConnectionInstance ci) {
 				checkConnDataPort(ci);
+				checkConnEventDataPort(ci);
 				checkConnSrc(ci);
 				checkConnDst(ci);
-				checkConnTiming(ci);				
+				checkConnTiming(ci);
 				return DONE;
 			}
 		};
 	}
-	
+
+	/************************/
 	private void checkCompSynch(ComponentInstance ci) {
-		if (ci.getCategory() != ComponentCategory.DATA)
+		if (ci.getCategory() != ComponentCategory.DATA && !PropertyUtil.isEnvironment(ci))
 		{
-			if(! PropertyUtil.isSynchronous(ci))
+			if(! PropertyUtil.isSynchronous(ci)) {
 				getErrorManager().error(ci, ci.getName() + " is not a synchronous " + ci.getCategory().getLiteral());
+			}
 		}
 	}
 
 	private void checkCompPeriod(ComponentInstance ci, ComponentInstance parent) {
-		if (ci.getCategory() != ComponentCategory.DATA)
+		if (ci.getCategory() != ComponentCategory.DATA && !PropertyUtil.isEnvironment(ci))
 		{
 			double period = GetProperties.getPeriodinMS(ci);
-			if (period == 0.0)
+			if (period == 0.0) {
 				getErrorManager().error(ci, ci.getName() + " does not define a period");
-			else
+			} else
 			{
-				if (parent != null && GetProperties.getPeriodinMS(parent) % period > 0)
+				if (parent != null && GetProperties.getPeriodinMS(parent) % period > 0) {
 					getErrorManager().error(ci, ci.getName() + " has a period that is not a multiple of its parent.");
+				}
 			}
 		}
 	}
 
 	private void checkThreadDispatchProtocol(ComponentInstance ci) {
-		if (ci.getCategory() == ComponentCategory.THREAD) {
+		if (ci.getCategory() == ComponentCategory.THREAD && !PropertyUtil.isEnvironment(ci)) {
 			EnumerationLiteral dp = GetProperties.getDispatchProtocol(ci);
-			if (dp == null || !dp.getName().equalsIgnoreCase(AadlProject.PERIODIC_LITERAL))
+			if (dp == null || !dp.getName().equalsIgnoreCase(AadlProject.PERIODIC_LITERAL)) {
 				getErrorManager().error(ci, ci.getName() + " is not periodic");
+			}
 		}
 	}
 
+	/*
+	 * private void checkCompEnv(ComponentInstance ci) {
+	 * if (ci.getCategory() == ComponentCategory.DATA && PropertyUtil.isEnvironment(ci)) {
+	 * GetProperties.getBaseType(ci);
+	 * if () {
+	 * getErrorManager().error(ci, ci.getName() + "");
+	 * }
+	 * }
+	 * }
+	 */
+	/************************/
 	private void checkConnDataPort(ConnectionInstance ci) {
-		if (!PropertyUtil.isDataPortConnection(ci))
-			getErrorManager().error(ci, ci.getName() + " is not an data port connection");
+		ComponentInstance src = PropertyUtil.getSrcComponent(ci);
+		ComponentInstance dst = PropertyUtil.getDstComponent(ci);
+		if (!PropertyUtil.isDataPortConnection(ci)) {
+			if ((PropertyUtil.isEnvironment(src) && PropertyUtil.isEnvironment(dst))
+					|| !PropertyUtil.isEnvironment(dst)) {
+				getErrorManager().error(ci, ci.getName() + " is not an data port connection");
+			}
+		}
+	}
+
+	private void checkConnEventDataPort(ConnectionInstance ci) {
+		ComponentInstance dst = PropertyUtil.getDstComponent(ci);
+		if (PropertyUtil.isEnvironment(dst)
+				&& (!PropertyUtil.isEventDataPortConnection(ci) || !PropertyUtil.isEventPortConnection(ci))) {
+			getErrorManager().error(ci, ci.getName() + " is not an event data / eventport connection");
+		}
 	}
 
 	private void checkConnSrc(ConnectionInstance ci) {
 		ComponentInstance src = PropertyUtil.getSrcComponent(ci);
-		if (src == null || src.getCategory() != ComponentCategory.THREAD) 
+		if (!PropertyUtil.isEnvironment(src) && (src == null || src.getCategory() != ComponentCategory.THREAD)) {
 			getErrorManager().error(ci, ci.getName() + " is invalid; src must be a non-null thread");
+		}
 	}
-	
+
 	private void checkConnDst(ConnectionInstance ci) {
 		ComponentInstance dst = PropertyUtil.getDstComponent(ci);
-		if (dst == null || dst.getCategory() != ComponentCategory.THREAD)
+		if (!PropertyUtil.isEnvironment(dst) && (dst == null || dst.getCategory() != ComponentCategory.THREAD)) {
 			getErrorManager().error(ci, ci.getName() + " is invalid; dst must be a non-null thread");
+		}
 	}
 
 	private void checkConnTiming(ConnectionInstance ci) {
-		if (! PropertyUtil.isDelayedPortConnection(ci))
-		{
-			getErrorManager().error(ci, ci.getName() + " is not delayed");
+		ComponentInstance src = PropertyUtil.getSrcComponent(ci);
+		ComponentInstance dst = PropertyUtil.getDstComponent(ci);
+		if (!PropertyUtil.isEnvironment(src) && !PropertyUtil.isEnvironment(dst)) {
+			if (!PropertyUtil.isDelayedPortConnection(ci)) {
+				getErrorManager().error(ci, ci.getName() + " is not delayed");
+			}
+		}
+		else {
+			if(PropertyUtil.isDelayedPortConnection(ci) || PropertyUtil.isImmediatePortConnection(ci)) {
+				getErrorManager().error(ci, ci.getName() + " is not non-deterministic");
+			}
 		}
 	}
-	
+
 
 }
--- edu.uiuc.aadl.synch/src/edu/uiuc/aadl/utils/PropertyUtil.java
+++ edu.uiuc.aadl.synch/src/edu/uiuc/aadl/utils/PropertyUtil.java
@@ -16,14 +16,14 @@ import org.osate.xtext.aadl2.properties.util.GetProperties;
 import org.osate.xtext.aadl2.properties.util.PropertyUtils;
 
 public class PropertyUtil {
-	
+
 	/*
 	 * Data Modeling Annex
 	 */
 	static public final String DATA_MODEL = "Data_Model";
 	static public final String DATA_REPRESENTATION = "Data_Representation" ;
 	static public final String INITIAL_VALUE = "Initial_Value" ;
-	
+
 	/*
 	 * MR-Synch-AADL property set
 	 */
@@ -31,90 +31,131 @@ public class PropertyUtil {
 	static public final String SYNCHRONOUS = "Synchronous";
 	static public final String NONDETERMINISTIC = "Nondeterministic";
 	static public final String INPUT_ADAPTOR = "InputAdaptor";
-	
-	
+
+	/*
+	 * Hybrid-AADL property set
+	 */
+	static public final String HYBRIDAADL = "HybridEnvironment";
+	static public final String ENVIRONMENT = "isEnvironment";
+	static public final String ODE = "ODE";
+
 	public static ListValue getDataInitialValue(final NamedElement ne) {
-		
+
 		try {
 			Property initialValue = GetProperties.lookupPropertyDefinition(ne, DATA_MODEL, INITIAL_VALUE);
 			return (ListValue) PropertyUtils.getSimplePropertyValue(ne, initialValue);
-		} 
+		}
 		catch (PropertyLookupException e) {
 			return null;
 		}
 	}
-	
+
 	public static boolean isSynchronous(ComponentInstance ci) {
-		return getBooleanProp(ci,SYNCHAADL,SYNCHRONOUS,false);
+		return getSynchBooleanProp(ci, SYNCHAADL, SYNCHRONOUS, false);
 	}
-	
-	
-	public static boolean getBooleanProp(final NamedElement ne, final String propertySet,
+
+
+	public static boolean getSynchBooleanProp(final NamedElement ne, final String propertySet,
 			final String propertyName, boolean defaultValue) {
 		try {
 			Property synchronous = GetProperties.lookupPropertyDefinition(ne, propertySet, propertyName);
 			return PropertyUtils.getBooleanValue(ne, synchronous);
-		} 
+		}
 		catch (PropertyLookupException e) {
 			return defaultValue;
 		}
 	}
-	
+
+	public static boolean isEnvironment(ComponentInstance ci) {
+		return getSynchBooleanProp(ci, HYBRIDAADL, ENVIRONMENT, false);
+	}
+
+	public static boolean getEnvBooleanProp(final NamedElement ne, final String propertySet, final String propertyName,
+			boolean defaultValue) {
+		try {
+			Property environment = GetProperties.lookupPropertyDefinition(ne, propertySet, propertyName);
+			return PropertyUtils.getBooleanValue(ne, environment);
+		} catch (PropertyLookupException e) {
+			return defaultValue;
+		}
+	}
+
 	public static boolean isPortConnection(final ConnectionInstance conn) {
 		return conn.getKind() == ConnectionKind.PORT_CONNECTION;
 	}
-	
+
 	public static boolean isDataPortConnection(final ConnectionInstance conn) {
 		ConnectionInstanceEnd cie = conn.getSource();
-		if (cie instanceof FeatureInstance)
+		if (cie instanceof FeatureInstance) {
 			return ((FeatureInstance) cie).getCategory() == FeatureCategory.DATA_PORT;
+		}
 		return false;
 	}
-	
+
+	public static boolean isEventDataPortConnection(final ConnectionInstance conn) {
+		ConnectionInstanceEnd cie = conn.getSource();
+		if (cie instanceof FeatureInstance) {
+			return ((FeatureInstance) cie).getCategory() == FeatureCategory.EVENT_DATA_PORT;
+		}
+		return false;
+	}
+
+	public static boolean isEventPortConnection(final ConnectionInstance conn) {
+		ConnectionInstanceEnd cie = conn.getSource();
+		if (cie instanceof FeatureInstance) {
+			return ((FeatureInstance) cie).getCategory() == FeatureCategory.EVENT_PORT;
+		}
+		return false;
+	}
+
 	public static EnumerationLiteral getConnectionTiming(final ConnectionInstance conn) {
 		try {
 			Property timing = GetProperties.lookupPropertyDefinition(conn, CommunicationProperties._NAME,
 					CommunicationProperties.TIMING);
 			return PropertyUtils.getEnumLiteral(conn, timing);
-		} 
+		}
 		catch (PropertyLookupException e) {
 			return null;
 		}
 	}
-	
+
 	public static boolean isDelayedPortConnection(final ConnectionInstance conn) {
 		if (isPortConnection(conn)) {
 			EnumerationLiteral el = getConnectionTiming(conn);
-			final String name = el.getName(); 
+			final String name = el.getName();
 			return name.equalsIgnoreCase(CommunicationProperties.DELAYED);
 		}
 		return false;
 	}
-	
+
 	public static boolean isImmediatePortConnection(final ConnectionInstance conn) {
 		if (isPortConnection(conn)) {
 			EnumerationLiteral el = getConnectionTiming(conn);
-			final String name = el.getName(); 
+			final String name = el.getName();
 			return name.equalsIgnoreCase(CommunicationProperties.IMMEDIATE);
 		}
 		return false;
 	}
-	
+
 	public static ComponentInstance getSrcComponent(final ConnectionInstance conni) {
 		ConnectionInstanceEnd srcEnd = conni.getSource();
-		if (srcEnd instanceof ComponentInstance)
+		if (srcEnd instanceof ComponentInstance) {
 			return (ComponentInstance) srcEnd;
-		if (srcEnd instanceof FeatureInstance)
+		}
+		if (srcEnd instanceof FeatureInstance) {
 			return srcEnd.getContainingComponentInstance();
+		}
 		return null;
 	}
-	
+
 	public static ComponentInstance getDstComponent(final ConnectionInstance conni) {
 		ConnectionInstanceEnd dstEnd = conni.getDestination();
-		if (dstEnd instanceof ComponentInstance)
+		if (dstEnd instanceof ComponentInstance) {
 			return (ComponentInstance) dstEnd;
-		if (dstEnd instanceof FeatureInstance)
+		}
+		if (dstEnd instanceof FeatureInstance) {
 			return dstEnd.getContainingComponentInstance();
+		}
 		return null;
 	}
 }
