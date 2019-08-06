957adacf5ba1055ac2f70bb93db9d428f3342dc1
gsojc234 <gsojc234@gmail.com>
2019-08-07 00:41:33 +0900

    Menu Updated

---
+++ SynchAadlMenu/src/synchaadlmenu/handlers/ConstraintChecker.java
@@ -0,0 +1,48 @@
+package synchaadlmenu.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.xtext.ui.editor.XtextEditor;
+
+import edu.uiuc.aadl.synch.checker.action.ConstrainsCheckAction;
+import edu.uiuc.aadl.synch.propspec.PropspecEditorResourceManager;
+
+public class ConstraintChecker extends AbstractHandler {
+
+	private Action checkAct;
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		PropspecEditorResourceManager res = new PropspecEditorResourceManager();
+		IWorkbenchPart part = HandlerUtil.getActivePart(event);
+
+		XtextEditor newEditor = (part.getSite().getId().compareTo("edu.uiuc.aadl.xtext.propspec.PropSpec") == 0)
+				&& (part instanceof XtextEditor) ? (XtextEditor) part : null;
+
+		res.setEditor(newEditor);
+		res.getModelResource();
+
+		checkAct = new Action("Constraints Check") {
+			@Override
+			public void run() {
+				if (res.getModelResource() != null) {
+					ConstrainsCheckAction chkAct = new ConstrainsCheckAction();
+					chkAct.selectionChanged(this, new StructuredSelection(res.getModelResource()));
+					chkAct.run(this);
+				} else {
+					System.out.println("No AADL instance model!");
+				}
+			}
+		};
+
+		checkAct.run();
+		return null;
+	}
+}
