e7fbe8be6a7ce776e492b5d9af395e5315ceda64
gsojc234 <gsojc234@gmail.com>
2020-01-19 19:54:22 +0900

    Unused folder removed

--- edu.postech.aadl.launch/src/edu/postech/aadl/launch/AADLLaunchConfigurationAttributes.java
+++
@@ -1,8 +0,0 @@
-package edu.postech.aadl.launch;
-
-public interface AADLLaunchConfigurationAttributes {
-	public static String MAUDE_OPTION = "-no-prelude prelude.maude smt.maude";
-	public static String PSPC_PATH = "";
-	public static String MAUDE_PATH = "";
-	public static String LAUNCH_CONFIGURATION_TYPE_ID = "edu.postech.aadl.launch.launchConfigurationType";
-}
